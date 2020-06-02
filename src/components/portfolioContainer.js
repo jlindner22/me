9fe728b9636435b819634426fae6c4da8415a99a
jlindner22 <jenlindner22@gmail.com>
2020-06-02 11:27:35 -0400

    Updates

--- src/components/portfolioContainer.js
+++
@@ -1,6 +0,0 @@
-import React from "react"
-import containerStyles from "./portfoliocontainer.module.css"
-
-export default function PortfolioContainer({ children }) {
-  return <div className={containerStyles.container}>{children}</div>
-}
